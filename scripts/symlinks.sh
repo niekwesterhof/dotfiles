264348de75f0567d5ac418edcb556f9224d17112
Niek <niekwesterhof@gmail.com>
2024-08-16 19:37:12 +0200

    update

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -6,28 +6,27 @@
 # make a directory for old config files to store
 cd ~
 mkdir -p old_config
-old_config=~/old_config
 
 # symlink for atuin
 mkdir -p ~/.config/atuin
-[ -f ~/.config/atuin/config.toml ] mv ~/.config/atuin/config.toml $old_config
+[ -f ~/.config/atuin/config.toml ] mv ~/.config/atuin/config.toml ~\config
 ln ~/dotfiles/atuin/config.toml ~/.config/atuin/config.toml
 
 # symlink for bash
-[ -f ~/.bashrc ] mv ~/.bashrc $old_config
+[ -f ~/.bashrc ] mv ~/.bashrc ~\config
 ln ~/dotfiles/bash/.bashrc ~/.bashrc
 
 # symlink for starship
-[ -f ~/.config/starship.toml ] mv ~/.config/starship.toml $old_config
+[ -f ~/.config/starship.toml ] mv ~/.config/starship.toml ~\config
 ln -s ~/dotfiles/startship/starship.toml ~/.config/starship.toml
 
 # symlink for zsh
-[ -f ~/.zshrc ] && mv ~/.zshrc $old_config
+[ -f ~/.zshrc ] && mv ~/.zshrc ~\config
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 
 # symlink for .ssh config
 mkdir -p ~/.ssh
-[ -f ~/.ssh/config ] && mv ~/.ssh/config $old_config
+[ -f ~/.ssh/config ] && mv ~/.ssh/config ~\config
 ln -s ~/dotfiles/.ssh/config ~/.ssh/config
 
 # symlink for i3
@@ -39,14 +38,14 @@ mkdir -p ~/.config/lazygit
 ln ~/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
 
 # symlink for nvim
-[ -d ~/.config/nvim] mv ~/.config/nvim $old_config
+[ -d ~/.config/nvim] mv ~/.config/nvim ~\config
 ln -s ~/dotfiles/nvim ~/.config/nvim
 
 # symlink for superfile
-[ -d ~/.config/superfile] mv ~/.config/superfile $old_config
+[ -d ~/.config/superfile] mv ~/.config/superfile ~\config
 ln -s ~/dotfiles/superfile ~/.config/superfile
 # symlink for tmux
-[ -f ~/.tmux.conf ] mv ~/.tmux.conf $old_config
+[ -f ~/.tmux.conf ] mv ~/.tmux.conf ~\config
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # symlink for ulauncher
@@ -54,5 +53,5 @@ ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/ulauncher ~/.config/ulauncher
 
 # symlink for yazi
-[ -d ~/.config/yazi ] mv ~/.config/yazi $old_config
+[ -d ~/.config/yazi ] mv ~/.config/yazi ~\config
 ln -s ~/dotfiles/yazi ~/.config/yazi
